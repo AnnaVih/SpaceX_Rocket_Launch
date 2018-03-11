76674478c31805bd204bcf4dcef80d84c63c607f
Anna Vihrogonova <annavihrogonova@yahoo.com>
2018-03-11 16:31:02 +0000

    1)Import classes and make instances of them. 2)Pass recieved data to method which will display in on UI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 require('bootstrap');
 
 require('./scss/style.scss');
+
+//Importing classes
+import { GetLaunches } from './getLatestLaunches';
+
+import { LastestLaunchesUI } from './latestLaunchesUI';
+
+
+const latestLaunchesUI = new LastestLaunchesUI();
+
+function getPhotoPosts() {
+    console.log('start getting');
+    //1. Make instance of GetPosts
+    const launches = new (GetLaunches);
+
+    //2.Getting data from promises
+    launches.get()
+              .then((postsResult) => {
+                latestLaunchesUI.displayPosts(postsResult);
+              })
+              //4.1 Catch error in case if no weather data recieved
+              .catch((err) => {
+                console.log(err);
+            });
+}
+
+
+/******************  EVENT LISTENERS  ***********************
+ ***********************************************************/
+
+//On load Event listeners
+document.addEventListener("DOMContentLoaded", getPhotoPosts);
